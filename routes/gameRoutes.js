3bbd9d321365c60914abacde51c3bb6c7e5f4285
Christian <christian_rose@live.ca>
2025-02-19 20:17:13 -0330

    Added genre validation middleware

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -4,14 +4,15 @@ const express = require('express');
 const router = express.Router();
 const gameController = require('../controllers/gameController');
 const { validateGameId } = require('../middleware/validateInputs');
+const validateGenre = require('../middleware/validateGenre');
 
-// Route to get games by genre
-router.get('/genre/:genre', gameController.getGamesByGenre);
+// Route to get games by genre (with genre validation)
+router.get('/genre/:genre', validateGenre, gameController.getGamesByGenre);
 
 // Route to get top-rated games
 router.get('/top-rated', gameController.getTopRatedGames);
 
-// Route to get game details by ID (with validation)
+// Route to get game details by ID (with ID validation)
 router.get('/:id', validateGameId, gameController.getGameDetails);
 
 // Route to get a random game
